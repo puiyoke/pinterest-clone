05531f52eb4c1eef9c4d8960328b19d2c07ad759
puiyoke <puiyoke8888@hotmail.com>
2018-05-25 16:27:34 +0800

    minor update users_controller - 'user' changed to '@user'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
-            if user.save
-                session[:user_id] = user.id
+        @user = User.new(user_params)
+            if @user.save
+                session[:user_id] = @user.id
                 flash[:notice] = "User Successfully Created"
                 redirect_to '/'
             else
                 flash[:notice] = "Unable to create user, please try again"
                 redirect_to '/signup'
             end
-    end   
+    end  
+    
+    def edit
+    end
 
     private
 
