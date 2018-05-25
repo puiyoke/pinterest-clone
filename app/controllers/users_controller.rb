6bab055b22db2e1969fe0c70b578590278f15e22
puiyoke <puiyoke8888@hotmail.com>
2018-05-25 16:13:42 +0800

    added gender selection to signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password)
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :gender)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
     has_secure_password
+    enum gender: {not_telling: 0, male: 1, female: 2}
 end
