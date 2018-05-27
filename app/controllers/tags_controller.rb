3de723bf9514865d59af00bf98e41eeabdd0a035
puiyoke <puiyoke8888@hotmail.com>
2018-05-27 18:27:30 +0800

    all submitted tags auto downcase to prevent case sensitivity, click on tags link shows all pictures under same tag

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,9 @@
+class TagsController < ApplicationController
+
+    def show
+        @tag = Tag.where(name: params[:tag])
+        @tagging = Tagging.where(tag_id: @tag)
+        @pin = Pin.all
+    end
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ApplicationRecord
     has_many :taggings
     has_many :pins, through: :taggings
+    before_save :downcase_fields
+
+    def downcase_fields
+       self.name.downcase!
+    end
 end
